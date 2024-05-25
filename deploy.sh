9905d13e768bdf11deda9630e083eeca0e3942fd
“dingqiangqiang” <1534713566@qq.com>
2024-05-26 01:18:19 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,11 @@ set -e
 npm run build
 
 cd docs/.vitepress/dist
-
+ 
 git init 
 git add -A
 git commit -m 'deploy' 
-git push -f https://github.com/dingqiangqiang/docs.git main
+git push -f https://github.com/dingqiangqiang/vitepress-docs.git main
 
 cd -
 rm -rf docs/.vitepress/dist
